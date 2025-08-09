838cb22ecd1cdadb54808c43121fec22ca71a817
Vladimir Valekzhanin <valdemarcheck@yandex.ru>
2025-08-10 00:31:04 +0300

    Use indices buffer

--- src/main.cpp
+++ src/main.cpp
@@ -21,6 +21,11 @@ GLfloat vertices[] =
     0.0f,
 };
 
+GLuint indices[] =
+{
+    0, 1, 2
+};
+
 void framebuffer_size_callback(GLFWwindow *window, int width, int height)
 {
     glViewport(0, 0, width, height);
@@ -33,7 +38,7 @@ void setupWindowHints() {
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 }
 
-GLFWwindow* createWindow(uint16_t width, uint16_t height, char* title) {
+GLFWwindow* createWindow(uint16_t width, uint16_t height, const char* title) {
     GLFWwindow* window = glfwCreateWindow(width, height, title, NULL, NULL);
 
     if (window == NULL)
@@ -80,26 +85,32 @@ int main()
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
+    // NOTE: VAO must be ALWAYS used *BEFORE* VBO in ALL commands
     GLuint VAO; // Vertex Array Object
     GLuint VBO; // Shader Buffer Object
+    GLuint EBO; // Index Buffer Object
 
-    // NOTE: VAO must be ALWAYS used *BEFORE* VBO in ALL commands
+    // Generate buffers with only 1 object each
     glGenVertexArrays(1, &VAO);
-
-    // Generate a buffer for each shader
     glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+
     // Make VAO the current vertex array object by binding it
     glBindVertexArray(VAO);
     // Bind the VBO specifying that it's a GL_ARRAY_BUFFER
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
-     // Add vertices into VBO
+    // Add vertices into VBO
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 
     glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
@@ -111,8 +122,8 @@ int main()
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
 
-        // Draws the triangle
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        // Draws the triangle from EBO
+        glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, 0);
 
         // Swaps front and back buffers of the window
         glfwSwapBuffers(window);
@@ -124,6 +135,7 @@ int main()
     // Cleanup
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
     glDeleteProgram(shaderProgram);
 
     glfwTerminate();
